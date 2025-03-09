df4ce7ba6e7dfbaab71d2f8b40d02307a307c509
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2025-03-09 18:30:16 +0500

    update main.py

--- Projects/number_guessing_game/main.py
+++ Projects/number_guessing_game/main.py
@@ -6,7 +6,7 @@ random_number = random.randrange(1, 30)
 
 chances: int = 5
 
-guess_counter: int = 0
+guess_counter: int = 0;
 
 while guess_counter < chances:
     guess_counter += 1
@@ -15,6 +15,8 @@ while guess_counter < chances:
     if my_guess == random_number:
         print(f"(You guess the correct number!! {random_number} in the {guess_counter} attempt)")
         break
+    elif guess_counter >= and chances   
+
 
 
 
