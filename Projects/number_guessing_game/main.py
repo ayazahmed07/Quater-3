2be9c4a586043492d5930931f45e051063c205de
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2025-03-09 18:15:55 +0500

    game

--- Projects/number_guessing_game/main.py
+++ Projects/number_guessing_game/main.py
@@ -1,2 +1,21 @@
 import random
 
+print("Welcome to the Number Guessing Game!! \n You got 5 attempts to guess the number between 1 and 30, lets start the game")
+
+random_number = random.randrange(1, 30)
+
+chances: int = 5
+
+guess_counter: int = 0
+
+while guess_counter < chances:
+    guess_counter += 1
+    my_guess = int(input("Enter your guess number: "))
+
+    if my_guess == random_number:
+        print("f(You guess the correct number {random_number})")
+
+
+
+
+print(random_number)
