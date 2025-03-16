37b1c0249ff063be10dcec69b0da2d63561f70b6
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2025-03-16 14:38:14 +0500

    Fix grammar in success message for number guessing game

--- Projects/number_guessing_game/main.py
+++ Projects/number_guessing_game/main.py
@@ -15,7 +15,7 @@ while guess_counter < chances:
     my_guess = int(input("Enter your guess number: "))
 
     if my_guess == random_number:
-        print(f"(You guess the correct number!! Your number is {random_number} and you guess in the {guess_counter} attempt!!)")
+        print(f"(You guess the correct number!! Your number is {random_number} and you guess in {guess_counter} attempt!!)")
         break
     elif guess_counter >= chances and my_guess != random_number:
         print(f"You failed to guess the number, the correct number is {random_number} better luck next time!!")
