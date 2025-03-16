742438945b1981acfd39e20cee443c52d361f24a
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2025-03-16 14:39:57 +0500

    Update random number range in number guessing game and fix grammar in success message

--- Projects/number_guessing_game/main.py
+++ Projects/number_guessing_game/main.py
@@ -6,7 +6,7 @@ chances: int = int(input("Enter your number of attempts: "))
 
 print(f"You got {chances} attempts to guess the number between 1 to 20")
 
-random_number = random.randrange(1, 10)
+random_number = random.randrange(1, 20)
 
 guess_counter: int = 0;
 
@@ -15,7 +15,7 @@ while guess_counter < chances:
     my_guess = int(input("Enter your guess number: "))
 
     if my_guess == random_number:
-        print(f"(You guess the correct number!! Your number is {random_number} and you guess in {guess_counter} attempt!!)")
+        print(f"(You guess the correct number!! Your number is {random_number} and you guess in {guess_counter} attempts!!)")
         break
     elif guess_counter >= chances and my_guess != random_number:
         print(f"You failed to guess the number, the correct number is {random_number} better luck next time!!")
