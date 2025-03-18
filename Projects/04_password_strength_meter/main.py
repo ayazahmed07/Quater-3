7a25cf19ddc21f5e03d0d3ce1c6d6bf746a7f4fc
Ayaz ahmed <ayazahmed824@gmail.com>
2025-03-18 07:38:21 +0000

    sidebar

---
+++ Projects/04_password_strength_meter/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.title("PASSWORD STRENGTH")
+
+st.sidebar.title("Sidebar")
+st.sidebar.write("This is a sidebar.")
+option = st.sidebar.radio("Select Option", ["Option1", "Option 2", "Option 3"])
+
+
