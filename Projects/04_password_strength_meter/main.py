9dd8d2de3edf3b048a75d4d2ae474a5149fc6db1
Ayaz ahmed <ayazahmed824@gmail.com>
2025-03-18 08:03:57 +0000

    password-generator

--- Projects/04_password_strength_meter/main.py
+++ Projects/04_password_strength_meter/main.py
@@ -13,6 +13,23 @@ def password_generator(length, use_digits, use_special):
     if use_special:
         characters += string.punctuation
     
+    return ''.join(random.choice(characters) for _ in range(length))
+
+length = st.slider("Select Password Length", min_value=8, max_value=14, value=12)
+
+use_digits = st.checkbox("Include Digits")
+
+use_special = st.checkbox("Include Specail Characters")
+
+if st.button("Generate Password"):
+
+    password = password_generator(length, use_digits, use_special)
+    st.subheader(f"Generated Password: {password}")
+    
+st.write("-------------------------------------")
+
+st.write("Build with love by Ayaz Ahmed")
+
 
 
 
