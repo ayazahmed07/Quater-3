75604743b238544e985564de605e54bb696056e6
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2025-04-13 19:10:52 +0500

    app

--- Projects/07_Secure_Data_Encryption_System_Using_Streamlit/main.py
+++ Projects/07_Secure_Data_Encryption_System_Using_Streamlit/main.py
@@ -1,9 +1,32 @@
 import streamlit as st
-
+from cryptography.fernet import Fernet
 import json
 import hashlib
 
+
+# Streamlit UI
+
 st.title("Secure Data Encryption System")
 
-menu = ["Home Page", "Store Data", "Retrieve Data", "Login"]
-choice = st.sidebar._selectbox("Navigation", menu)
+# Navigation
+menu = ["Home", "Store Data", "Retrieve Data", "Login"]
+choice = st.sidebar.selectbox("Navigation", menu)
+
+if choice == "Home":
+    st.subheader("🏠 Welcome to the Secure Data System")
+    st.write("Use this app to **securely store and retrieve data** using unique passkeys.")
+
+elif choice == "Store Data":
+    st.subheader("Store Your Data")
+    user_data = st.text_area("Enter Your Data")
+    passkey = st.text_input("Enter your pass key:", type="password")
+
+    if st.button("Encrypt & Save"):
+        if user_data and passkey:
+            hashed_passkey = hash_passkey(passkey)
+            encrypted_data = encrypt_data(data)
+            st.success("Data Stored Successfuly!")
+        else:
+            st.error("Please enter both data and passkey")
+
+    
